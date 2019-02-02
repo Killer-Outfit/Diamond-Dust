3252fe1525b94a44b0e1eb6468e2f6e3f13dce25
unknown <eblank@ucsc.edu>
2019-02-02 11:58:44 -0800

    added the player and camera

---
+++ Assets/Evan-Scripts/FollowCamera.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    //public GameObject target;
+    public Transform target;
+    public float minDistance;
+    public float maxDistance;
+    public float camHeight;
+    public float XrotateSpeed;
+    public float YrotateSpeed;
+    Vector3 offset;
+    private float x;
+    private float y;
+
+    Vector3 targetDir;
+    public GameObject lockMarker;
+    Vector3 hiddenMarker;
+
+    GameObject lockTarget;
+    bool lockOn = false;
+
+    private Transform myTransform;
+
+    void Start()
+    {
+        if (target == null)
+            Debug.LogWarning("no target selected");
+        myTransform = transform;
+        CameraSetup();
+        lockTarget = findClosest();
+        hiddenMarker = new Vector3(0, -200, 0);
+    }
+
+    //using late update so camera moves after player moves in update
+    void LateUpdate()
+    {
+        if(Input.GetAxis("rightTrigger") > 0 && !lockOn)
+        {
+            Debug.Log("MADEITTTTTTGFKUYTYFIYSFUYKDGUYKDFKUDY");
+            lockTarget = findClosest();
+            lockOn = true;
+        }else if(Input.GetAxis("rightTrigger") == 0 && lockOn)
+        {
+            lockMarker.transform.position = hiddenMarker;
+            lockOn = false;
+            x = myTransform.rotation.x;
+            y = myTransform.rotation.y;
+        }
+        if (!lockOn)
+        {
+            x += Input.GetAxis("RStick X") * XrotateSpeed;
+            y -= Input.GetAxis("RStick Y") * YrotateSpeed;
+            
+            var rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -minDistance) + target.position;
+
+            myTransform.rotation = rotation;
+            myTransform.position = position;
+        }else
+        {
+            targetDir = lockTarget.transform.position - transform.position;
+            // The step size is equal to speed times frame time.
+            float step = 40 * Time.deltaTime;
+            var targetRotation = Quaternion.LookRotation(lockTarget.transform.position - transform.position);
+            Vector3 position = targetRotation * new Vector3(0.0f, 0.0f, -minDistance) + target.position;
+            myTransform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, step);
+            myTransform.position = position;
+            Vector3 markerPos = new Vector3(lockTarget.transform.position.x, lockTarget.transform.position.y + 2, lockTarget.transform.position.z);
+            lockMarker.transform.position = markerPos;
+        }
+    }
+
+    public GameObject findClosest()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float closestDist = 0;
+        float enemyDist = 0;
+        foreach (GameObject enemy in enemies)
+        {
+            if(closest == null)
+            {
+                closest = enemy;
+                closestDist = getDistance(enemy);
+            }
+            enemyDist = getDistance(enemy);
+            Debug.Log("Enemy = " + enemy.name + " distance = " + enemyDist);
+            if(enemyDist < closestDist)
+            {
+                closest = enemy;
+                closestDist = enemyDist;
+            }
+        }
+        
+        return closest;
+    }
+
+    public float getDistance(GameObject enemy)
+    {
+        float distance = Vector3.Distance(enemy.transform.position, target.transform.position);
+        return distance;
+    }
+
+    public void CameraSetup()
+    {
+        myTransform.position = new Vector3(target.position.x, target.position.y + camHeight, target.position.z - minDistance);
+        myTransform.LookAt(target);
+    }
+
+}
---
+++ Assets/Evan-Scripts/PlayerMove.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float movementSpeed;
+    public float turningSpeed;
+    float lockSpeed;
+    Quaternion targetRotation;
+    //public GameObject child; //used with child relative motion
+
+    //use with cam relative motion 2
+    public Transform camPivot;
+    public Transform cam;
+    float heading;
+    //
+
+    bool isLock;
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        isLock = false;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //get stick inputs
+        float horizontal = Input.GetAxis("LStick X") * movementSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("LStick Y") * movementSpeed * Time.deltaTime;
+
+        Debug.Log("horiz = " + horizontal + " vert = " + vertical);
+
+        //cam2 movement
+        heading += horizontal;
+        camPivot.rotation = Quaternion.Euler(0, heading, 0);
+        Vector2 inputs = new Vector2(horizontal, vertical);
+        inputs = Vector2.ClampMagnitude(inputs, 1);
+        Vector3 camF = cam.forward;
+        Vector3 camR = cam.right;
+        camF.y = 0f;
+        camR.y = 0f;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+
+        if (Input.GetAxis("rightTrigger") > 0 && !isLock)
+        {
+            //isLock = true;
+        }
+        else if (Input.GetAxis("rightTrigger") == 0 && isLock)
+        {
+            isLock = false;
+        }
+
+        if (!isLock)
+        {
+            transform.position += (camF * inputs.y + camR * inputs.x) * Time.deltaTime * movementSpeed;
+
+            //setting character rotation
+            if (inputs.x != 0 || inputs.y != 0)
+            {
+                var rotation = Quaternion.LookRotation(((camF * inputs.y + camR * inputs.x) * Time.deltaTime * movementSpeed));
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turningSpeed);
+            }
+            
+        }
+        else
+        {
+            //
+            if (System.Math.Abs(horizontal) < vertical)
+            {
+                lockSpeed = movementSpeed;
+            }else
+            {
+                lockSpeed = 10;
+            }
+            //Debug.Log("lockspeed = " + lockSpeed);
+            transform.rotation = Quaternion.LookRotation(camF);
+            transform.position += (camF * inputs.y + camR * inputs.x) * Time.deltaTime * lockSpeed;
+            
+        }
+            //
+        //
+
+        //play run animation when the player is moving
+        if (vertical != 0 || horizontal != 0)
+        {
+            anim.Play("HumanoidRun");
+        }
+        else
+        {
+            anim.SetTrigger("stopRun");
+        }
+        //
+
+
+        /*
+        //Code to establish camera relative movement
+        var camera = Camera.main;
+        var camFor = camera.transform.forward;
+        var camRight = camera.transform.right;
+
+        camFor.y = 0f;
+        camRight.y = 0f;
+
+        camFor.Normalize();
+        camRight.Normalize();
+
+        var relativeDirection = camFor * vertical + camRight * horizontal;
+        */
+        /*
+        //check to see if user in pushing a stick
+        var input = new Vector3(horizontal, 0, vertical);
+        if (input != Vector3.zero)
+        {
+            //set rotation vectors
+            transform.forward = input;
+            targetRotation = Quaternion.LookRotation(input);
+
+            //trigger the run animation
+            anim.SetTrigger("run");
+            
+        }
+        else {
+            //end the run animation
+            anim.SetTrigger("stopRun");
+        }
+
+        //set rotation
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, movementSpeed * Time.deltaTime);
+
+        //child relative motion
+        //transform.position = child.transform.position;
+
+        //world relative motion
+        //transform.Translate(horizontal, 0, vertical, Space.World);
+
+        //camera relative motion
+        //transform.Translate(relativeDirection * movementSpeed * Time.deltaTime);
+
+        horizontal = 0;
+        vertical = 0;
+        */
+    }
+}
