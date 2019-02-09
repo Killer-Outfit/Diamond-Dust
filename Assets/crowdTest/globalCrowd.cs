4dc8659280cdd3c960d774b21e28ecb6db9be568
SpessMareen <eramitch@ucsc.edu>
2019-02-08 18:45:43 -0800

    Edited my code to follow the Code Style format document

--- Assets/crowdTest/globalCrowd.cs
+++ Assets/crowdTest/globalCrowd.cs
@@ -6,13 +6,14 @@ public class globalCrowd : MonoBehaviour
 {
     public GameObject crowdPrefab;
     public static int arenaSize = 1000;
-    // public GameObject playerPrefab;
+    //public GameObject playerPrefab;
     public static int numCrowd = 1000;
     public static GameObject[] allCrowd = new GameObject[numCrowd];
-    // private static Vector3 pPos = Vector3.zero;
-
-    // public static int spaceHeight = 1960;
+    //public static int spaceHeight = 1960;
     public static Vector3 goalPos = Vector3.zero;
+
+    //private static Vector3 pPos = Vector3.zero
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,7 @@ public class globalCrowd : MonoBehaviour
             Vector3 pos = new Vector3(Random.Range(-arenaSize, arenaSize),
                                       0,
                                       Random.Range(-arenaSize, arenaSize));
-            // Debug.Log("SpaceHeight = " + pos.y);
+            //Debug.Log("SpaceHeight = " + pos.y);
             allCrowd[i] = (GameObject) Instantiate(crowdPrefab, pos, Quaternion.identity);
         }
         
@@ -32,14 +33,14 @@ public class globalCrowd : MonoBehaviour
     {
         if(Random.Range(0,10000) < 50)
         {
-            // Vector3 pRange = new Vector3(pPos.x + 10, 0, pPos.z + 10);
-            // while (goalPos.x <= pRange.x || goalPos.z <= pRange.z)
-            // {
+            //Vector3 pRange = new Vector3(pPos.x + 10, 0, pPos.z + 10);
+            //while (goalPos.x <= pRange.x || goalPos.z <= pRange.z)
+            //{
                 goalPos = new Vector3(Random.Range(-arenaSize, arenaSize),
                                       0,
                                       Random.Range(-arenaSize, arenaSize));
             //Debug.Log("new SpaceHeight = " + goalPos.y);
-            // }
+            //}
         }
     }
 }
