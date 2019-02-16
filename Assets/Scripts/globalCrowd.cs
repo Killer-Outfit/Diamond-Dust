3d24436055089ac9a1353ea48bf123ee6d5a8fe5
Chubblezap <ajwiley@ucsc.edu>
2019-02-16 02:44:33 -0800

    Resolve Merge Conflict

--- Assets/Scripts/globalCrowd.cs
+++ Assets/Scripts/globalCrowd.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class globalCrowd : MonoBehaviour
 {
     public GameObject crowdPrefab;
-<<<<<<< HEAD
-    public static int arenaSize = 1000;
-    // public GameObject playerPrefab;
-=======
     public static int arenaSize = 100;
     public GameObject playerPrefab;
->>>>>>> develop
     public static int numCrowd = 50;
     public static GameObject[] allCrowd = new GameObject[numCrowd];
     //public static int spaceHeight = 1960;
