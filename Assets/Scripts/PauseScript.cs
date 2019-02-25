bc114569f8230dae035f9505a3c39fe6b4d559f8
SpessMareen <eramitch@ucsc.edu>
2019-02-24 18:55:02 -0800

    Enabled OutfitMenu to be Accessed from Pause Menu (outfit swapping still not implemented)

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class PauseScript : MonoBehaviour
 {
     GameObject menu;
+    GameObject outfitMenu;
+    GameObject playerHealth;
 	GameObject changeTop;
 	GameObject close;
+    GameObject mainCanvas;
+    GameObject mainCamera;
+    Camera cam;
+    Canvas outfitCanvas;
 	
     bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = GameObject.Find("Main Camera");
+        cam = mainCamera.GetComponent<Camera>();
         menu = GameObject.Find("PauseMenuElements");
+        playerHealth = GameObject.Find("Player Health");
+        outfitMenu = GameObject.Find("OutfitMenuElements");
+        mainCanvas = GameObject.Find("MainCanvas");
+        outfitCanvas = mainCanvas.GetComponent<Canvas>();
         menu.SetActive(false);
+        outfitMenu.SetActive(false);
         isPaused = false;
     }
 
@@ -24,9 +37,10 @@ public class PauseScript : MonoBehaviour
         {
             // Enable Pause Menu
 			menu.SetActive(true);
-            
-			// Disable Gameplay
-			Time.timeScale = 0.0f;
+            //playerHealth.
+
+            // Disable Gameplay
+            Time.timeScale = 0.0f;
             isPaused = true;
             Time.timeScale = 0.0f;
         }else if (Input.GetButtonDown("StartButton") && isPaused)
@@ -41,6 +55,21 @@ public class PauseScript : MonoBehaviour
         }
     }
 	
+    public void OpenOutfitMenu()
+    {
+        menu.SetActive(false);
+        outfitMenu.SetActive(true);
+        outfitCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+        outfitCanvas.worldCamera = cam;
+    }
+
+    public void CloseOutfitMenu()
+    {
+        outfitMenu.SetActive(false);
+        menu.SetActive(true);
+        outfitCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+    }
+
 	// Loop Through Children
 	/*
 	void enableChildren(GameObject target)
