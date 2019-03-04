7cd837f6486ba67d2ef2389a73c071348d229aa6
YerffoegT <46553990+YerffoegT@users.noreply.github.com>
2019-03-03 19:02:36 -0800

    Attempted to Integrate Dynamic Music
    WIP

---
+++ Assets/Scripts/Sounds and Music/JukeBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JukeBox : MonoBehaviour
+{
+	public FMOD.Studio.EventInstance areaMusic;
+	public FMOD.Studio.ParameterInstance battleParam;
+	[FMODUnity.EventRef]
+	    public string areaMusicEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        areaMusic = FMODUnity.RuntimeManager.CreateInstance(areaMusicEvent);
+		areaMusic.getParameter("InBattle", out battleParam);
+        areaMusic.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+		{
+			battleMusic(1.0f);
+		}
+    }
+	
+	public void battleMusic(float inBattle)
+	{
+		battleParam.setValue(1.0f);
+		//Debug.Log(battleParam.);
+	}
+}
