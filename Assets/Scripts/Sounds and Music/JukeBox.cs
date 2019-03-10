d160313d6099de3c37fa691ef9e97244a31a3ab2
YerffoegT <46553990+YerffoegT@users.noreply.github.com>
2019-03-10 16:01:26 -0700

    Added JukeBox and example usage scene
    Music integration

--- Assets/Scripts/Sounds and Music/JukeBox.cs
+++ Assets/Scripts/Sounds and Music/JukeBox.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class JukeBox : MonoBehaviour
 {
-	public FMOD.Studio.EventInstance areaMusic;
-	public FMOD.Studio.ParameterInstance battleParam;
 	[FMODUnity.EventRef]
-	    public string areaMusicEvent;
+	public string musicEventName;//Set this in the editor to the event that corrosponds to the music you want to play
+		
+	//A float between zero and one. Set to 1 during combat, and to 0 when outside combat
+	public float InBattle
+	{
+		get 
+		{
+			float val;
+			inBattle.getValue(out val);
+			return val;
+		}
+		set {inBattle.setValue(value);}
+	}
+	
+	
+	private FMOD.Studio.EventInstance musicEventInstance;//this is the runtime object that will play the music
+	private FMOD.Studio.ParameterInstance inBattle;//this is the instance of the music event's parameter "In Battle"
+	
     // Start is called before the first frame update
     void Start()
     {
-        areaMusic = FMODUnity.RuntimeManager.CreateInstance(areaMusicEvent);
-		areaMusic.getParameter("InBattle", out battleParam);
-        areaMusic.start();
+        musicEventInstance = FMODUnity.RuntimeManager.CreateInstance(musicEventName);//create the runtime object that plays the music
+		musicEventInstance.getParameter("In Battle", out inBattle);//get its parameter "In Battle"
+        musicEventInstance.start();//play the music!
     }
 
     // Update is called once per frame
@@ -21,13 +36,7 @@ public class JukeBox : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
 		{
-			battleMusic(1.0f);
+			InBattle = 1.0f;
 		}
     }
-	
-	public void battleMusic(float inBattle)
-	{
-		battleParam.setValue(1.0f);
-		//Debug.Log(battleParam.);
-	}
 }
